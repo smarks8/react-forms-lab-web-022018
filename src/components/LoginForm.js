ba726cf61c33cafc86187e5d03a2838a4d2d1f4f
smarks8 <staceyamarks@gmail.com>
2018-04-25 13:55:10 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,66 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+    };
   }
+
+  handleInputChange = event => {
+   const { name, value } = event.target;
+// {name: "password"}
+// {value: "pas"}
+    console.log(event.target)
+    console.log(name);
+    console.log(value);
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const { username, password } = this.state;
+    // {username: this.state.username}
+    //const username = this.state.username
+
+    if (!username || !password) {
+      return;
+    }
+
+    this.props.onSubmit({ username, password });
+  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleFormSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input
+              id="test-username"
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input
+              id="test-password"
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
@@ -29,5 +70,9 @@ class LoginForm extends React.Component {
     );
   }
 }
+
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
 
 export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
+//read more about proptypes here: https://reactjs.org/docs/typechecking-with-proptypes.html
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = { message: "" };
   }
+
+  setMessage = event => {
+    this.setState({ message: event.target.value });
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.message}
+          onChange={this.setMessage}
+        />
+        <p>{this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
 }
+
+
+
+TwitterMessage.defaultProps = {
+  maxChars: 140,
+};
 
 export default TwitterMessage;
